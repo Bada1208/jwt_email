0d6dc87bd3adac6ef02544f4ea073ccf3a8aeccc
Bada1208 <sysoievbogdan@gmail.com>
2021-01-18 09:07:52 +0100

    Initial commit

---
+++ src/main/java/com/sysoiev/jwt_email/JwtEmailApplication.java
@@ -0,0 +1,13 @@
+package com.sysoiev.jwt_email;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtEmailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sysoiev/jwt_email/JwtEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sysoiev.jwt_email;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtEmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
